99c4e0e0b8b45568f75b0456fb10ad8be0df30a0
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2025-02-17 10:44:43 -0300

    04.Requisição DELETE
    - Fazendo algumas melhorias no codigo e na UI.

--- js/api.js
+++ js/api.js
@@ -1,9 +1,11 @@
+const URL_BASE = "http://localhost:3000";
+
 const api = {
 
     // Read
     async buscarPensamentos() {
         try {
-            const response = await fetch('http://localhost:3000/pensamentos');
+            const response = await fetch(`${URL_BASE}/pensamentos`);
             return await response.json();
         } catch (error) {
             alert("Erro ao buscar pensamentos");
@@ -14,7 +16,7 @@ const api = {
     // Create
     async salvarPensamento(pensamento) {
         try {
-            const response = await fetch('http://localhost:3000/pensamentos', {
+            const response = await fetch(`${URL_BASE}/pensamentos`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
@@ -31,7 +33,7 @@ const api = {
     // Update
     async buscarPensamentoPorId(id) {
         try {
-            const response = await fetch(`http://localhost:3000/pensamentos/${id}`);
+            const response = await fetch(`${URL_BASE}/pensamentos/${id}`);
             return await response.json();
         } catch (error) {
             alert("Erro ao buscar pensamento");
@@ -41,7 +43,7 @@ const api = {
 
     async editarPensamento(pensamento) {
         try {
-            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+            const response = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`, {
                 method: "PUT",
                 headers: {
                     "Content-Type": "application/json"
@@ -58,7 +60,7 @@ const api = {
     // Delete
     async excluirPensamento(id) {
         try {
-            const response = await fetch(`http://localhost:3000/pensamentos/${id}`, {
+            const response = await fetch(`${URL_BASE}/pensamentos/${id}`, {
                 method: "DELETE"
             });
         } catch (error) {
